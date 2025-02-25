fc62ed224b762481f91fa136dc200d9d41a0fb9f
hhehasj <alfredlennardaquino@gmail.com>
2025-02-25 18:05:03 +0700

    Game_window appears, and the previous windows disappears.

--- Guessing_Game/game_window.py
+++ Guessing_Game/game_window.py
@@ -7,3 +7,34 @@ class Game_Window(ctk.CTk):
 
         self.title("Game On")
         self.geometry("920x550")
+
+        self.greeting_lbl = ctk.CTkLabel(
+            self,
+            text="GUESSING GAME!!!",
+            bg_color="green",
+            font=("Helvetica", 75, "bold"),
+            text_color="white",
+            corner_radius=20,
+        )
+        self.greeting_lbl.place(
+            relx=0.5, rely=0.3, anchor="center", relwidth=0.9, relheight=0.15
+        )
+
+        Guesses = ctk.IntVar()
+        self.input_field = ctk.CTkEntry(self,
+                                        placeholder_text="Enter your guesses here.",
+                                        textvariable=Guesses,
+                                        border_color="black",
+                                        border_width=2,
+                                        font=("Helvetica", 20, "bold"))
+        self.input_field.place(relx=0.4, rely=0.7, anchor="center",
+                               relwidth=0.4, relheight=0.08)
+
+        # self.mechanics_lbl =
+        # self.mechanics_lbl.place()
+        #
+        # self.submit_btn =
+        # self.submit_btn.place()
+
+    def start_loop(self):
+        self.mainloop()
