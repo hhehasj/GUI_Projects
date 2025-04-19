11a76f5f0aa3ccc89291f09095d064276d2332c0
hhehasj <alfredlennardaquino@gmail.com>
2025-04-19 21:33:35 +0700

    Initial commit of Automate_Google_Sheets

---
+++ Automate_Google_Sheets/main_window.py
@@ -0,0 +1,8 @@
+import PySide6.QtWidgets as Widgets
+import PySide6.QtCore as Core
+import PySide6.QtGui as Gui
+
+
+class MainWindow(Widgets.QWidget):
+    def __init__(self):
+        super().__init__()
